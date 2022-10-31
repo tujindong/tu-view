99fd8e2a835ed45e8e7d6364fe45b45566d43663
凃锦东 <1965382331@qq.com>
2022-10-31 18:33:08 +0800

    add button style

--- examples/App.vue
+++ examples/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
-    <tu-button class="demo-button" type="default">默认按钮</tu-button>
-    <tu-button class="demo-button" type="primary">主要按钮</tu-button>
-    <tu-button class="demo-button" type="info">信息按钮</tu-button>
-    <tu-button class="demo-button" type="success">成功按钮</tu-button>
-    <tu-button class="demo-button" type="warning">警告按钮</tu-button>
-    <tu-button class="demo-button" type="danger">危险按钮</tu-button>
+    <div class="demo-button-wrapper">
+      <tu-button class="demo-button" type="default">按钮</tu-button>
+      <tu-button class="demo-button" type="default">默认按钮</tu-button>
+      <tu-button class="demo-button" type="primary">主要按钮</tu-button>
+      <tu-button class="demo-button" type="info">信息按钮</tu-button>
+      <tu-button class="demo-button" type="success">成功按钮</tu-button>
+      <tu-button class="demo-button" type="warning">警告按钮</tu-button>
+      <tu-button class="demo-button" type="danger">危险按钮</tu-button>
+    </div>
   </div>
 </template>
 
@@ -20,8 +23,11 @@ export default {};
   font-family: "Poppins", sans-serif;
   background: #e4ebf5;
 
-  .demo-button {
-    margin-left: 30px;
+  .demo-button-wrapper {
+    padding-top: 20px;
+    .demo-button {
+      margin-left: 30px;
+    }
   }
 }
 </style>
