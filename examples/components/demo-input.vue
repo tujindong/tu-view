0164d418a94d11a225688a6b7248282c22772e04
凃锦东 <1965382331@qq.com>
2022-11-18 14:04:44 +0800

    add input component

--- examples/components/demo-input.vue
+++ examples/components/demo-input.vue
@@ -18,8 +18,32 @@
     <tu-input v-model="input" placeholder="请输入内容" size="large"></tu-input>
 
     <br />
+    <p>禁用</p>
+    <tu-input
+      v-model="inputDisabled1"
+      disabled
+      placeholder="请输入内容"
+    ></tu-input>
+    <br />
+
+    <p>禁用带输入值</p>
+    <tu-input
+      v-model="inputDisabled2"
+      disabled
+      placeholder="请输入内容"
+    ></tu-input>
+
+    <br />
     <p>可清空</p>
     <tu-input v-model="input1" placeholder="请输入内容" clearable></tu-input>
+
+    <br />
+    <p>带密码</p>
+    <tu-input
+      v-model="input1"
+      placeholder="请输入密码"
+      show-password
+    ></tu-input>
   </div>
 </template>
 
@@ -30,6 +54,8 @@ export default {
   data() {
     return {
       input: "",
+      inputDisabled1: "",
+      inputDisabled2: "禁用带输入值",
       input1: "",
     };
   },
--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -10,12 +10,34 @@
   >
     <input
       ref="input"
-      :tabindex="tabindex"
       class="tu-input__inner"
-      type="type"
-      :placeholder="placeholder"
+      v-bind="$attrs"
+      :tabindex="tabindex"
+      :type="type"
+      :disabled="isDisabled"
+      :readonly="readonly"
+      :aria-label="label"
+      @compositionstart="handleCompositionStart"
+      @compositionupdate="handleCompositionUpdate"
+      @compositionend="handleCompositionEnd"
+      @change="handleChange"
+      @input="handleInput"
+      @focus="handleFocus"
+      @blur="handleBlur"
     />
-    <span class="tu-input__suffix"> </span>
+    <!-- 前置内容 -->
+    <span class="tu-input__prefix"></span>
+    <!-- 后置内容 -->
+    <span class="tu-input__suffix">
+      <span class="tu-input__suffix-inner">
+        <i
+          v-if="showClear"
+          class="tu-input__icon tu-icon-close-circle-fill tu-input__clear"
+          @mousedown.prevent
+          @click="handleClear"
+        ></i>
+      </span>
+    </span>
   </div>
 </template>
 
@@ -28,11 +50,12 @@ export default {
   inheritAttrs: false,
 
   props: {
-    placeholder: String,
     value: [String, Number],
     size: String,
     disabled: Boolean,
     readonly: Boolean,
+    label: String,
+    tabindex: String,
     type: {
       type: String,
       default: "text",
@@ -41,13 +64,17 @@ export default {
       type: Boolean,
       default: false,
     },
-    tabindex: String,
+    showPassword: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   data() {
     return {
       hovering: false,
       focused: false,
+      isComposing: false,
     };
   },
 
@@ -57,6 +84,85 @@ export default {
     },
     isDisabled() {
       return this.disabled;
+    },
+    showClear() {
+      return (
+        this.clearable &&
+        !this.isDisabled &&
+        !this.readonly &&
+        this.nativeInputValue &&
+        (this.focused || this.hovering)
+      );
+    },
+    nativeInputValue() {
+      return this.value !== null || this.value !== undefined
+        ? String(this.value)
+        : "";
+    },
+  },
+
+  watch: {
+    nativeInputValue() {
+      this.setNativeInputValue();
+    },
+  },
+
+  mounted() {
+    this.setNativeInputValue();
+  },
+
+  methods: {
+    getInput() {
+      return this.$refs.input;
+    },
+
+    setNativeInputValue() {
+      const input = this.getInput();
+      if (!input) return;
+      if (input.value === this.nativeInputValue) return;
+      input.value = this.nativeInputValue;
+    },
+
+    handleCompositionStart(evt) {
+      this.$emit("compositionstart", evt);
+      this.isComposing = true;
+    },
+
+    handleCompositionUpdate(evt) {
+      this.$emit("compositionupdate", evt);
+    },
+
+    handleCompositionEnd(evt) {
+      this.$emit("compositionend", evt);
+      if (this.isComposing) {
+        this.isComposing = false;
+        this.handleInput(evt);
+      }
+    },
+
+    handleInput(evt) {
+      if (this.isComposing) return;
+      this.$emit("input", evt.target.value);
+      this.$nextTick(this.setNativeInputValue);
+    },
+
+    handleChange(evt) {
+      this.$emit("change", evt.target.value);
+    },
+
+    handleFocus(evt) {
+      this.focused = true;
+      this.$emit("focus", evt);
+    },
+
+    handleBlur(evt) {
+      this.focused = false;
+      this.$emit("blur", evt);
+    },
+
+    handleClear() {
+      this.$emit("input", "");
+      this.$emit("change", "");
     },
   },
 };
