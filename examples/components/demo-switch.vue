4e4d45700b349a1d77f4a594ebe0623ca6216ae6
凃锦东 <1965382331@qq.com>
2022-11-23 13:06:06 +0800

    add switch component

--- examples/components/demo-switch.vue
+++ examples/components/demo-switch.vue
@@ -3,8 +3,28 @@
     <h2>切换按钮</h2>
     <br />
     <p>基本切换</p>
-    <tu-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
-    </tu-switch>
+    <tu-switch v-model="value"></tu-switch>
+
+    <br />
+    <p>禁用</p>
+    <tu-switch v-model="value1" disabled></tu-switch>
+    <tu-switch v-model="value2" disabled></tu-switch>
+
+    <br />
+    <p>超小</p>
+    <tu-switch v-model="value" size="mini"></tu-switch>
+
+    <br />
+    <p>较小</p>
+    <tu-switch v-model="value" size="small"></tu-switch>
+
+    <br />
+    <p>较大</p>
+    <tu-switch v-model="value" size="large"></tu-switch>
+
+    <br />
+    <p>图标</p>
+    <tu-switch v-model="value" activeLabel="开" inactiveLabel="关"></tu-switch>
   </div>
 </template>
 
@@ -15,6 +35,8 @@ export default {
   data() {
     return {
       value: true,
+      value1: false,
+      value2: true,
     };
   },
 };
--- packages/components/switch/src/switch.vue
+++ packages/components/switch/src/switch.vue
@@ -1,11 +1,42 @@
 <template>
   <div
     class="tu-switch"
-    :class="{ 'is-disabled': isDisabled, 'is-checked': checked }"
+    :class="[
+      switchSize ? `tu-switch--${switchSize}` : '',
+      { 'is-disabled': isDisabled },
+      { 'is-checked': checked },
+    ]"
+    role="switch"
+    :aria-checked="checked"
+    :aria-disabled="isDisabled"
+    @click.prevent="handleValueSwitch"
   >
     <div class="tu-switch__input">
-      <input class="tu-switch__original" type="checkbox" />
+      <input
+        ref="input"
+        class="tu-switch__original"
+        type="checkbox"
+        :id="id"
+        :name="name"
+        :true-value="activeValue"
+        :false-value="inactiveValue"
+        :disabled="isDisabled"
+        @change="handleChange"
+        @keydown.enter="handleValueSwitch"
+      />
+      <span
+        v-if="($slots.activeLabel || activeLabel) && checked"
+        :class="['tu-switch__label', 'tu-switch__label--left']"
+      >
+        <span v-if="activeLabel">{{ activeLabel }}</span>
+      </span>
       <span class="tu-switch__core" ref="core"></span>
+      <span
+        v-if="($slots.inactiveLabel || inactiveLabel) && !checked"
+        :class="['tu-switch__label', 'tu-switch__label--right']"
+      >
+        <span v-if="inactiveLabel">{{ inactiveLabel }}</span>
+      </span>
     </div>
   </div>
 </template>
@@ -15,15 +46,69 @@ export default {
   name: "TuSwitch",
 
   props: {
+    id: String,
+    size: String,
+    value: {
+      type: [Boolean, String, Number],
+      default: false,
+    },
     disabled: {
       type: Boolean,
       default: false,
     },
+    activeLabel: String,
+    inactiveLabel: String,
+    activeValue: {
+      type: [Boolean, String, Number],
+      default: true,
+    },
+    inactiveValue: {
+      type: [Boolean, String, Number],
+      default: false,
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return {};
   },
 
   computed: {
+    checked() {
+      return this.value === this.activeValue;
+    },
+    switchSize() {
+      return this.size;
+    },
     isDisabled() {
       return this.disabled;
+    },
+  },
+
+  created() {
+    if (!~[this.activeValue, this.inactiveValue].indexOf(this.value)) {
+      this.$emit("input", this.inactiveValue);
+    }
+  },
+
+  mounted() {},
+
+  methods: {
+    handleChange(evt) {
+      const value = this.checked ? this.inactiveValue : this.activeValue;
+      this.$emit("input", value);
+      this.$emit("change", value);
+      this.$nextTick(() => {
+        if (this.$refs.input) {
+          this.$refs.input.checked = this.checked;
+        }
+      });
+    },
+    handleValueSwitch() {
+      !this.isDisabled && this.handleChange();
     },
   },
 };
