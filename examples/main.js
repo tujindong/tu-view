4582356137011f0f1286eb6fbc9e1396eae6420e
凃锦东 <1965382331@qq.com>
2023-04-17 19:03:20 +0800

    docs: 添加国际化示例文档

--- examples/main.js
+++ examples/main.js
@@ -1,20 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-// import tuview from "../packages/tuview";
-// import locale from "../lib/src/locale/lang/en";
-import "./theme-dark.scss";
-
-import Calendar from "../lib/packages/calendar";
-import "../lib/packages/calendar/style.css";
-import lang from "../lib/src/locale/lang/en";
-import locale from "../lib/src/locale";
-
-// 设置语言
-locale.use(lang);
-
-// 引入组件
-Vue.component(Calendar.name, Calendar);
-// Vue.use(tuview.install, { locale });
+import tuview from "../lib/tuview";
+import "../lib/tuview.css";
+Vue.use(tuview.install);
 
 Vue.config.productionTip = false;
 
