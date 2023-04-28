c39278ded58d0aef26e280966f26c30b29454d0b
凃锦东 <1965382331@qq.com>
2023-04-28 18:54:05 +0800

    feat: 添加时钟组件

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <tu-clock></tu-clock>
+    <div style="width: 200px; height: 200px">
+      <tu-clock></tu-clock>
+      <tu-clock time="10:20:33:55"></tu-clock>
+    </div>
   </div>
 </template>
 
--- packages/components/clock/src/clock.vue
+++ packages/components/clock/src/clock.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="tu-clock">
-    <div class="tu-clock--hour"></div>
-    <div class="tu-clock--min"></div>
-    <div class="tu-clock--sec"></div>
+    <div
+      class="tu-clock__hand is-hours"
+      :style="{ transform: hourRotate }"
+    ></div>
+    <div
+      class="tu-clock__hand is-minutes"
+      :style="{ transform: minuteRotate }"
+    ></div>
+    <div
+      class="tu-clock__hand is-seconds"
+      :style="{ transform: secondRotate }"
+    ></div>
+    <div class="tu-clock__inner">
+      <div
+        class="tu-clock__inner--marker"
+        v-for="marker in markers"
+        :key="marker"
+      >
+        <span
+          ><i>{{ marker }}</i></span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,15 +30,52 @@
 export default {
   name: "TuClock",
 
-  props: {},
+  props: {
+    time: String,
+  },
 
   data() {
-    return {};
+    return {
+      markers: [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      hourRotate: "rotatez(0deg)",
+      minuteRotate: "rotatez(0deg)",
+      secondRotate: "rotatez(0deg)",
+    };
   },
 
   computed: {},
 
-  methods: {},
+  created() {},
+
+  mounted() {
+    if (this.timer) clearInterval(this.timer);
+    if (!this.timer) {
+      this.timer = setInterval((_) => this.showTime(), 1000);
+    }
+  },
+
+  destroyed() {
+    if (this.timer) clearInterval(this.timer);
+  },
+
+  methods: {
+    showTime() {
+      const now = new Date();
+      const times = this.time
+        ? this.time.split(":")
+        : [now.getHours(), now.getMinutes(), now.getSeconds()];
+      let hour = +times[0];
+      let minute = +times[1];
+      let second = +times[2] || 0;
+      hour = hour > 11 ? hour - 12 : hour;
+      let hourAngle = hour * 30 + ((minute * 6) / 360) * 30;
+      let minuteAngle = minute * 6;
+      let secondAngle = second * 6;
+      this.hourRotate = `rotatez(${hourAngle}deg)`;
+      this.minuteRotate = `rotatez(${minuteAngle}deg)`;
+      this.secondRotate = `rotatez(${secondAngle}deg)`;
+    },
+  },
 };
 </script>
 
