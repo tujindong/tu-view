e9a91b48fa0b466f98be22f8623586883a501788
凃锦东 <1965382331@qq.com>
2023-05-22 09:30:49 +0800

    chore: vue3.0组件库初始化

--- packages/entry_config.js
+++
@@ -1,9 +0,0 @@
-// 分离打包时的入口配置文件
-module.exports = {
-    configList: [
-        {
-            name: 'button',
-            author: 'tuu'
-        },
-    ]
-}
--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    // 这个工具可以实现自动添加CSS3前缀
-		"autoprefixer": {}
-  }
-}
