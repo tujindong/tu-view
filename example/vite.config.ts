52cb55729916297fea263b8a62b5757f5848d36f
凃锦东 <1965382331@qq.com>
2023-05-22 14:50:14 +0800

    chore: 添加组件库脚手架，打包，发布脚本

---
+++ example/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// @ts-ignore
+import DefineOptions from "unplugin-vue-define-options/vite";
+
+export default defineConfig({
+  plugins: [vue(), DefineOptions()],
+});
